ecc051bd71c9238918de5dceb40292160e343bc8
leyashi <chiper322@gmail.com>
2024-11-11 12:01:31 +0200

    исправленная версия

--- js/app.js
+++ js/app.js
@@ -1,15 +1,50 @@
 if (window.jQuery) {
-  let result = $(".calculate .result");
-  $( ".nums li" ).on( "click", function() {
-    console.log("work", this);
-    let clickedText = $(this).text();
-    let number = Number(clickedText);
-    if (!isNaN(number) ||clickedText == "." ) {
-      $(result).text($(result).text() + clickedText);
-    }
-    console.log(result);
-  } );
-} else {
-  var Nk = 'neok';
-  console.log(Nk);
+	let result = $(".calculate .result");
+
+	$(".nums li").on("click", function () {
+
+		let clickedText = $(this).text();
+
+		switch (clickedText){
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+				$('.calculate .request .first').text( result.text() );
+				$('.calculate .request .symbol').text(clickedText);
+				result.text('');
+				break;
+
+			case '=':
+				let first = $('.calculate .request .first').text(),
+					symbol = $('.calculate .request .symbol').text();
+				if(
+					first.length !== 0
+					&& symbol.length !== 0
+					&& result.text().length !== 0
+				){
+					$('.calculate .request .second').text(result.text());
+					result.text( eval( first + symbol + result.text() ) );
+				}
+				break;
+
+			default:
+				if (result.text().indexOf('.') !== -1 && clickedText === '.') {
+					return false;
+				}
+
+				if (result.text().length === 0 && clickedText === '.') {
+					clickedText = "0" + clickedText;
+				}
+
+				let number = Number(clickedText);
+				if (
+					!isNaN(number)
+					|| clickedText === '.'
+				) {
+					$(result).text( $(result).text() + clickedText );
+				}
+				break;
+		}
+	});
 }
